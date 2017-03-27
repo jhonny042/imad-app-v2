d2713dba9f8cc3683d5ff0f826fb39a890f1cb7e
jhonny042 <mukulkumar042@gmail.com>
2017-03-27 23:08:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/mukul.png', function (req, res) {
+app.get('/ui/mukul.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
