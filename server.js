be2525a7f32763def63ad36b7241dffe57e1da33
jhonny042 <mukulkumar042@gmail.com>
2017-03-28 01:11:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/calc', function (req, res) {
-  res.send('welcome to calculator');
+  res.sendFile(path.join(__dirname, 'ui', 'calc.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
